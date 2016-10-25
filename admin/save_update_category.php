5d87e2672369bad745eb1d5489ff865f06814008
Kirill <extazy1701@gmail.com>
2016-10-25 21:22:56 +0300

    Delete save_update_category.php

--- admin/save_update_category.php
+++
@@ -1,7 +0,0 @@
-﻿<?
-require('../Models/ViewGetToolsModel.php');
-$name=$_GET['category'];
-$id=$_GET['id_category'];
-$dbcon=ViewGetTools::connect();
-$res=$dbcon->exec("UPDATE categories SET name='$name' WHERE category_id='$id'");
-?>
