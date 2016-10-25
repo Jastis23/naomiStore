16157462f2822907002ac1b6b6e02de387cd630b
Kirill <extazy1701@gmail.com>
2016-10-25 21:22:01 +0300

    Delete delete_save_prod.php

--- admin/delete_save_prod.php
+++
@@ -1,8 +0,0 @@
-﻿<?php 
-include('../Models/ViewGetToolsModel.php');
-$dbcon=ViewGetTools::connect();
-$id=$_GET['id'];
-$res=$dbcon->exec("DELETE FROM products WHERE id='$id'");
-echo"Добавлено<br>";
-?>
-<a href="index.php">На главную</a>
